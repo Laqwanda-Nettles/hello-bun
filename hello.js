0839de26e48175b36cebf20017ce77bb3c285ab5
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2026-07-30 21:42:30 -0500

    start hello-bun with hello script and setup notes

---
+++ hello.js
@@ -0,0 +1,4 @@
+let course = "Level 2";
+let message = "Hello from " + course + "!";
+console.log(message);
+console.log("This Javascript runs on My machine.");
---
+++ notes.md
@@ -0,0 +1,5 @@
+# My Setup
+- OS: Windows 25H2
+- Shell: Git Bash
+- Bun version: 1.3.14
+- Editor: Notepad++
