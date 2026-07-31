01f5bcbcaec56febed3541956847c879aab83779
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2026-07-30 22:36:48 -0500

    added a console log that prints the date

--- hello.js
+++ hello.js
@@ -2,3 +2,7 @@ let course = "Level 2";
 let message = "Hello from " + course + "!";
 console.log(message);
 console.log("This Javascript runs on My machine.");
+
+//Print Date
+console.log("Today's Date is: ");
+console.log(new Date().toDateString());
